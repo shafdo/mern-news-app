8b1e9e5874b02a3d1b5d2fe81d562de975bcd2c7
Shalinda Fernando <yugantha1468@gmail.com>
2023-10-24 02:03:57 +0530

    FEAT: Added user CRUD functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,26 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import config from './config';
+import apiRoutes from './routes';
+import connectDB from './database';
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+// Routes
+app.use('/api', apiRoutes);
+connectDB();
 
 app.listen(config.PORT, () => {
   console.log('Server is listening on port 3000');
